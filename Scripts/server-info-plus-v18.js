564c1c10a56185608038739318d733b6a339eff8
okamichen <webmaster@88188.live>
2023-06-05 15:55:58 +0800

    Update and rename server-info-plus-v17.js to server-info-plus-v18.js

--- Scripts/server-info-plus-v18.js
+++ Scripts/server-info-plus-v18.js
@@ -1,4 +1,6 @@
-var url = "http://ip234.in/ip.json"
+let base_url = "http://ip234.in/fraud_check?ip="
+
+var url = "https://api.ip.sb/geoip"
 var opts = {
     policy: $environment.params
 };
@@ -9,38 +11,26 @@ var myRequest = {
 };
 
 var message = ""
-const paras = ["ip","organization","country_code","continent","city","zip"]
-const paran = ["IP","ISP","地区","大洲","城市","邮编"]
+const paras = ["ip","isp","country_code","city"]
+const paran = ["IP","ISP","地区","城市"]
 $task.fetch(myRequest).then(response => {
-    message = response? json2info(response.body.data,paras) : ""
+    message = response? json2info(response.body,paras) : ""
     let ip = JSON.parse(response.body)["ip"]
-    var myRequest2 = {
-        url: "https://api.ipdata.co/?api-key=e2591b3a85fca5a39e04c34f530fc8d4b82400ff70df867b67eb3681&ip="+ip,
+    var myRequest1 = {
+        url: base_url+ip,
         opts: opts,
-        timeout: 3000
+        timeout: 4000
     };
-    var myRequest3 = {
-        url: "https://ip234.in/fraud_check?ip="+ip,
-        opts: opts,
-        timeout: 3000
-    };
-
-    let split ="------------------------------"+"</br>"
-
-    $task.fetch(myRequest2).then(resp => {
-        $task.fetch(myRequest3).then(obj => {
-            message = message + Display(JSON.parse(resp.body), JSON.parse(obj.body))
-            message = message+ split+"<font color=#6959CD>"+"<b>节点</b> ➟ " + $environment.params+ "</font>"
-            message =  `<p style="text-align: center; font-family: -apple-system; font-size: large; font-weight: thin">` + message + `</p>`
-            $done({"title": "🔎  查询结果", "htmlMessage": message});
-        }),reason => {
-            message = `<p style="text-align: center; font-family: -apple-system; font-size: large; font-weight: bold;">` + message + `</p>`
-            $done({"title": "🔎  查询结果", "htmlMessage": message});
-        }
-    }),reason => {
+    $task.fetch(myRequest1).then(response => {
+        message = message + Display(response.body)
+        message = message+ "------------------------------"+"</br>"+"<font color=#6959CD>"+"<b>节点</b> ➟ " + $environment.params+ "</font>"
+        message =  `<p style="text-align: center; font-family: -apple-system; font-size: large; font-weight: thin">` + message + `</p>`
+        $done({"title": "🔎  查询结果", "htmlMessage": message});
+    }, reason => {
+        message = "</br></br>🛑 查询超时"
         message = `<p style="text-align: center; font-family: -apple-system; font-size: large; font-weight: bold;">` + message + `</p>`
         $done({"title": "🔎  查询结果", "htmlMessage": message});
-    }
+    })
 }, reason => {
     message = "</br></br>🛑 查询超时"
     message = `<p style="text-align: center; font-family: -apple-system; font-size: large; font-weight: bold;">` + message + `</p>`
@@ -59,16 +49,11 @@ function json2info(cnt,paras) {
     return res
 }
 
-function Display(data, obj) {
-    let risk = ""
-    risk = risk + "</br><b>"+ "<font  color=>" +"I P " + "</font> : " + "</b>"+ "<font  color=>"+data["ip"]+"</font></br>"
-    risk = risk + "</br><b>"+ "<font  color=>" +"代理 " + "</font> : " + "</b>"+ "<font  color=>"+data["threat"]["is_proxy"]+"</font></br>"
-    risk = risk + "</br><b>"+ "<font  color=>" +"恶意 " + "</font> : " + "</b>"+ "<font  color=>"+data["threat"]["is_known_attacker"]+"</font></br>"
-    risk = risk + "</br><b>"+ "<font  color=>" +"滥用 " + "</font> : " + "</b>"+ "<font  color=>"+data["threat"]["is_known_abuser"]+"</font></br>"
-    risk = risk + "</br><b>"+ "<font  color=>" +"类型 " + "</font> : " + "</b>"+ "<font  color=>"+data["asn"]["type"]+"</font></br>"
-    risk = risk + "</br><b>"+ "<font  color=>" +"风险 " + "</font> : " + "</b>"+ "<font  color=>"+obj["data"]["risk"]+"</font></br>"
-    risk = risk + "</br><b>"+ "<font  color=>" +"得分 " + "</font> : " + "</b>"+ "<font  color=>"+obj["data"]["score"]+"</font></br>"
-    return risk
+function Display(resp) {
+    let body = JSON.parse(resp)
+    let score = "</br><b>"+ "<font  color=>" +"欺诈 " + "</font> : " + "</b>"+ "<font  color=>"+ body.risk +"</font></br>"
+    let risk = "</br><b>"+ "<font  color=>" +"风险 " + "</font> : " + "</b>"+ "<font  color=>"+ body.score +"</font></br>"
+    return (score+risk)
 }
 
 var flags = new Map([[ "AC" , "🇦🇨" ] ,["AE","🇦🇪"], [ "AF" , "🇦🇫" ] , [ "AI" , "🇦🇮" ] , [ "AL" , "🇦🇱" ] , [ "AM" , "🇦🇲" ] , [ "AQ" , "🇦🇶" ] , [ "AR" , "🇦🇷" ] , [ "AS" , "🇦🇸" ] , [ "AT" , "🇦🇹" ] , [ "AU" , "🇦🇺" ] , [ "AW" , "🇦🇼" ] , [ "AX" , "🇦🇽" ] , [ "AZ" , "🇦🇿" ] , ["BA", "🇧🇦"], [ "BB" , "🇧🇧" ] , [ "BD" , "🇧🇩" ] , [ "BE" , "🇧🇪" ] , [ "BF" , "🇧🇫" ] , [ "BG" , "🇧🇬" ] , [ "BH" , "🇧🇭" ] , [ "BI" , "🇧🇮" ] , [ "BJ" , "🇧🇯" ] , [ "BM" , "🇧🇲" ] , [ "BN" , "🇧🇳" ] , [ "BO" , "🇧🇴" ] , [ "BR" , "🇧🇷" ] , [ "BS" , "🇧🇸" ] , [ "BT" , "🇧🇹" ] , [ "BV" , "🇧🇻" ] , [ "BW" , "🇧🇼" ] , [ "BY" , "🇧🇾" ] , [ "BZ" , "🇧🇿" ] , [ "CA" , "🇨🇦" ] , [ "CF" , "🇨🇫" ] , [ "CH" , "🇨🇭" ] , [ "CK" , "🇨🇰" ] , [ "CL" , "🇨🇱" ] , [ "CM" , "🇨🇲" ] , [ "CN" , "🇨🇳" ] , [ "CO" , "🇨🇴" ] , [ "CP" , "🇨🇵" ] , [ "CR" , "🇨🇷" ] , [ "CU" , "🇨🇺" ] , [ "CV" , "🇨🇻" ] , [ "CW" , "🇨🇼" ] , [ "CX" , "🇨🇽" ] , [ "CY" , "🇨🇾" ] , [ "CZ" , "🇨🇿" ] , [ "DE" , "🇩🇪" ] , [ "DG" , "🇩🇬" ] , [ "DJ" , "🇩🇯" ] , [ "DK" , "🇩🇰" ] , [ "DM" , "🇩🇲" ] , [ "DO" , "🇩🇴" ] , [ "DZ" , "🇩🇿" ] , [ "EA" , "🇪🇦" ] , [ "EC" , "🇪🇨" ] , [ "EE" , "🇪🇪" ] , [ "EG" , "🇪🇬" ] , [ "EH" , "🇪🇭" ] , [ "ER" , "🇪🇷" ] , [ "ES" , "🇪🇸" ] , [ "ET" , "🇪🇹" ] , [ "EU" , "🇪🇺" ] , [ "FI" , "🇫🇮" ] , [ "FJ" , "🇫🇯" ] , [ "FK" , "🇫🇰" ] , [ "FM" , "🇫🇲" ] , [ "FO" , "🇫🇴" ] , [ "FR" , "🇫🇷" ] , [ "GA" , "🇬🇦" ] , [ "GB" , "🇬🇧" ] , [ "HK" , "🇭🇰" ] ,["HU","🇭🇺"], [ "ID" , "🇮🇩" ] , [ "IE" , "🇮🇪" ] , [ "IL" , "🇮🇱" ] , [ "IM" , "🇮🇲" ] , [ "IN" , "🇮🇳" ] , [ "IS" , "🇮🇸" ] , [ "IT" , "🇮🇹" ] , [ "JP" , "🇯🇵" ] , [ "KR" , "🇰🇷" ] , [ "LU" , "🇱🇺" ] , [ "MO" , "🇲🇴" ] , [ "MX" , "🇲🇽" ] , [ "MY" , "🇲🇾" ] , [ "NL" , "🇳🇱" ] , [ "PH" , "🇵🇭" ] , [ "RO" , "🇷🇴" ] , [ "RS" , "🇷🇸" ] , [ "RU" , "🇷🇺" ] , [ "RW" , "🇷🇼" ] , [ "SA" , "🇸🇦" ] , [ "SB" , "🇸🇧" ] , [ "SC" , "🇸🇨" ] , [ "SD" , "🇸🇩" ] , [ "SE" , "🇸🇪" ] , [ "SG" , "🇸🇬" ] , [ "TH" , "🇹🇭" ] , [ "TN" , "🇹🇳" ] , [ "TO" , "🇹🇴" ] , [ "TR" , "🇹🇷" ] , [ "TV" , "🇹🇻" ] , [ "TW" , "🇨🇳" ] , [ "UK" , "🇬🇧" ] , [ "UM" , "🇺🇲" ] , [ "US" , "🇺🇸" ] , [ "UY" , "🇺🇾" ] , [ "UZ" , "🇺🇿" ] , [ "VA" , "🇻🇦" ] , [ "VE" , "🇻🇪" ] , [ "VG" , "🇻🇬" ] , [ "VI" , "🇻🇮" ] , [ "VN" , "🇻🇳" ] , [ "ZA" , "🇿🇦"]])
